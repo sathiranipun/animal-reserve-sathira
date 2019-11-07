49e501848c6289a7c8d6b640c8ac4420b3626ca6
sathiranipun <sathiranipun.vsn@gmail.com>
2019-11-07 13:20:47 +0530

    fgasfgdgh

--- animal-reserve-sathira/recognizer.js
+++ animal-reserve-sathira/recognizer.js
@@ -1,7 +1,8 @@
 let AWS = require("aws-sdk");
+const ddb = new AWS.DynamoDB.DocumentClient();
 let rekog = new AWS.Rekognition();
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
     //console.log(JSON.stringify(event, null, 2));
 
     let s3 = event.Records[0].s3;
@@ -14,9 +15,22 @@ exports.handler = function(event, context, callback) {
         },
         MaxLabels: 1
     }).promise()
-    .then(data => {
-        console.log(data);
-        callback(null, {});
-    })
-    .catch(err => callback(err));
+        .then(data => {
+            console.log(data);
+
+            ddb.put({
+                TableName: 'animal',
+                Item: { 'name': s3.object.key, 'label': label }
+            }).promise()
+                .then((data) => {
+                    //your logic goes here
+                })
+                .catch((err) => {
+                    //handle error
+                });
+
+
+            callback(null, {});
+        })
+        .catch(err => callback(err));
 }
