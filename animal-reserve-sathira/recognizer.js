f0fb3999aa26a46cdd2ee9732fcecdf97dcdd906
sathiranipun <sathiranipun.vsn@gmail.com>
2019-11-07 12:55:29 +0530

    API calling

--- animal-reserve-sathira/recognizer.js
+++ animal-reserve-sathira/recognizer.js
@@ -1,4 +1,22 @@
+let AWS = require("aws-sdk");
+let rekog = new AWS.Rekognition();
+
 exports.handler = function(event, context, callback) {
-    console.log(JSON.stringify(event,null,2));
-    callback(null, {"message": "Successfully executed"});
+    //console.log(JSON.stringify(event, null, 2));
+
+    let s3 = event.Records[0].s3;
+    rekog.detectLabels({
+        Image: {
+            S3Object: {
+                Bucket: s3.bucket.name,
+                Name: s3.object.key
+            }
+        },
+        MaxLabels: 1
+    }).promise()
+    .then(data => {
+        console.log(data);
+        callback(null, {});
+    })
+    .catch(err => callback(err));
 }
